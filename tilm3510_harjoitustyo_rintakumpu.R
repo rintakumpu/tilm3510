29f79b0bac3dac8682addfa0f3e1eca69bf76900
Lasse Rintakumpu <rintakumpu@gmail.com>
2013-12-18 18:48:24 +0200

    Add talous data + empty script file

---
+++ tilm3510_harjoitustyo_rintakumpu.R
@@ -0,0 +1,8 @@
+# TILM3510 -harjoitustyö, R-koodi
+# Lasse Rintakumpu, 63555
+# 18.12.2013
+
+setwd("D:/Dropbox/Edu/Statistics/Peruskurssi B");
+pankki <- read.csv("D:/Dropbox/Edu/Statistics/Peruskurssi B/pankki.csv", sep=",", dec=",");
+
+file.path()
